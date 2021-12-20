73ef528635855a9c7621b77b6f557ca71f16dc5b
Antoine Lamontagne <antoine.lamontagne@polymtl.ca>
2021-12-19 21:37:16 -0500

    christmas

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-hashtag = 'COVID19'
+hashtag = 'christmas'
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
